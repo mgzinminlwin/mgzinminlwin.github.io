f236427f9ee1c48ddc90e8ef3d9773289b406069
goaltvapp <76167290+goaltvapp@users.noreply.github.com>
2024-03-20 09:20:37 +0630

    Create main.py

---
+++ MyM3U/main.py
@@ -0,0 +1,25 @@
+playlist_entries = [
+    {
+        "tvg-logo": "http://localhost:8080/maharbhawdi.png",
+        "group-title": "MM",
+        "title": "Mahar Bhawdi",
+        "url": "https://ppradio.b-cdn.net/LiveApp/streams/rHEBIW7pjQLU1677679374164.m3u8"
+    },
+    {
+        "tvg-logo": "http://localhost:8080/mrtv4.png",
+        "group-title": "MM",
+        "title": "MRTV-4",
+        "url": "https://ppradio.b-cdn.net/LiveApp/streams/w3g3EYjBtqgJ1677679288037.m3u8"
+    }
+]
+
+playlist = "#EXTM3U\n"
+
+for entry in playlist_entries:
+    playlist += f"#EXTINF:-1 tvg-logo=\"{entry['tvg-logo']}\" group-title=\"{entry['group-title']}\", {entry['title']}\n{entry['url']}\n"
+
+# Write the playlist to a file
+with open("playlist.m3u", "w") as file:
+    file.write(playlist)
+
+print("Playlist created successfully!")
